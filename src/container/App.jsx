2b453da9ee5b1f2352384f04e608e2f6b83094be
camilo <velandiacamilo356@gmail.com>
2020-04-16 21:22:56 -0500

    add readme

---
+++ src/container/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>My template</h1>
+      <p>By: Camilo Velandia || @xander_coder</p>
+    </div>
+  );
+};
+
+export default App;
